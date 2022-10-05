9716e24026b0b49e357904a9c5e4427eb15f3626
imOscarCrespo <oscarcrespo5@gmail.com>
2022-10-05 19:19:05 +0200

    first release

--- onetoc/asgi.py
+++ onetoc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for badel project.
+ASGI config for onetoc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetoc.settings')
 
 application = get_asgi_application()
--- onetoc/wsgi.py
+++ onetoc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for badel project.
+WSGI config for onetoc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetoc.settings')
 
 application = get_wsgi_application()
