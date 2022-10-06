53f969a25fae7317da55bad9e87c73a7f9bb94b3
imOscarCrespo <oscarcrespo5@gmail.com>
2022-10-06 16:05:14 +0200

    try

--- onetoc/settings.py
+++ onetoc/settings.py
@@ -97,7 +97,7 @@ if 'RDS_DB_NAME' in os.environ:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'onetoc',
-            'USER': 'oscarcrespo',
+            'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],  
