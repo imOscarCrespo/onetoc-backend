0222d2695776ad7c886044b33e93e3e0947f90d1
oscar <oscar@MBP-de-Oscar-2.home>
2022-10-23 10:27:43 +0200

    match get by id

--- api/urls.py
+++ api/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('match', MatchListApiView.as_view()),
     path('action', ActionListApiView.as_view()),
     re_path('action/(?P<id>\d+)/', ActionListApiView.as_view(), name='action'),
+    re_path('match/(?P<id>\d+)/', MatchListApiView.as_view(), name='match_by_id'),
     path('timeline', TimelineListApiView.as_view()),
     path('csrf', CsrfApiView.as_view()),
     path('logout', LogoutView.as_view())
