918300dfff533757d5112cc6356bc9a0718ec1d6
oscar <oscar@MBP-de-Oscar-2.home>
2022-10-28 16:31:47 +0200

    remove slash

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('team', TeamListApiView.as_view()),
     path('match', MatchListApiView.as_view()),
     path('action', ActionListApiView.as_view()),
-    re_path('action/(?P<id>\d+)/', ActionListApiView.as_view(), name='action'),
-    re_path('match/(?P<id>\d+)/', MatchListApiView.as_view(), name='match_by_id'),
+    re_path('action/(?P<id>\d+)', ActionListApiView.as_view(), name='action'),
+    re_path('match/(?P<id>\d+)', MatchListApiView.as_view(), name='match_by_id'),
     path('timeline', TimelineListApiView.as_view()),
     path('csrf', CsrfApiView.as_view()),
     path('logout', LogoutView.as_view())
