3970a45ce0c5f522ecab8d5382b5cb7d5abebfcd
oscar <oscar@MBP-de-Oscar-2.home>
2023-01-06 10:57:24 +0100

    fix: tab view

--- api/views.py
+++ api/views.py
@@ -229,12 +229,23 @@ class ActionListApiView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TabListApiView(APIView):
-    permission_classes = (IsAuthenticated)
-
     def get(self, request, *args, **kwargs):
-        tabs = Tab.objects.get().order_by('order')
+        tabs = Tab.objects.all().order_by('order')
         serializer = TabSerializer(tabs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.data.get('name'),
+            'icon': request.data.get('name'),
+            'order': request.data.get('order'),
+        }
+        serializer = TabSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TimelineListApiView(APIView):
 
