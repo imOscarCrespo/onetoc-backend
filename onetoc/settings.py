8b0b97fc9337f208ad0b1ddbdfa255afda48d1ed
oscar <oscar@oscars-MacBook-Pro.local>
2024-10-14 22:27:44 +0200

    fix: settings

--- onetoc/settings.py
+++ onetoc/settings.py
@@ -177,7 +177,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Add this if not already present
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
